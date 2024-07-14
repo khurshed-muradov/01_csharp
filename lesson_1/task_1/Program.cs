4b9e853677ca3d3ad646baf3230e1efc1d8cd613
Khurshed Muradov <hurshed@gmail.com>
2024-07-14 13:28:04 +0500

    added some variables lesson_1:task_1

--- lesson_1/task_1/Program.cs
+++ lesson_1/task_1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+
+int value = 100;
+System.Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+
+System.Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
