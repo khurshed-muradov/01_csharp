328c5ff63e6b573815d3dafbf31abb015898c744
Khurshed Muradov <hurshed@gmail.com>
2024-07-23 07:13:12 +0500

    added lesson_2 task_2

---
+++ lesson_2/task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+while (i < arr.Length) //while (i < n)
+{
+    arr[i] = i + 1;
+    //System.Console.WriteLine(arr[i]);
+    //System.Console.WriteLine(" ");
+    System.Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
