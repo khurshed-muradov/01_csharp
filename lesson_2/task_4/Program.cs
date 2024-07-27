f9b9eed052e70226727233a7e24479e5d9b2348d
Khurshed Muradov <hurshed@gmail.com>
2024-07-27 23:31:10 +0500

    added for lessons

--- lesson_2/task_4/Program.cs
+++ lesson_2/task_4/Program.cs
@@ -1,13 +1,28 @@
-﻿int n = 5;
-int[] array = { 2, 4, 7, 3, 9 };
-int i = 0;
-int max = array[0];
-while (i < n)
-{
-    if (array[i] > max)
-    {
-        max = array[i];
-    }
-    i = i + 1;
-}
-System.Console.WriteLine(max);
+﻿// int n = 5;
+// int[] array = { 2, 4, 7, 3, 9 };
+// int i = 0;
+// int max = array[0];
+// while (i < n)
+// {
+//     if (array[i] > max)
+//     {
+//         max = array[i];
+//     }
+//     i = i + 1;
+// }
+// System.Console.WriteLine(max);
+
+
+// int[] arr = { 1, 2, 3, 4, 5 };
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     System.Console.WriteLine($"{arr[i]} ");
+// }
+
+// int n = 100;
+// int sum = 0;
+// for (int i = 0; i < n; i++)
+// {
+//     sum = sum + i;
+// }
+// System.Console.WriteLine(sum);
