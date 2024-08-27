646f8e99137e16c758a284d2521962449cab90cc
Khurshed Muradov <hurshed@gmail.com>
2024-08-27 21:04:38 +0500

    added lasson 3 task 1

---
+++ lesson_3/task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denuminator = c + d;
+    double result = numinator / denuminator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
